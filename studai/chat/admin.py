c96c6b8ff8e502c177694304b9b8710df6f4b26c
qmayker <qmayker9@gmail.com>
2026-05-26 18:26:11 +0300

    ADD: Chat, Message models to admin, made migrations for them

--- studai/chat/admin.py
+++ studai/chat/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Chat, Message
 
 # Register your models here.
+
+class MessageInline(admin.TabularInline):
+    model = Message
+    extra = 0
+
+@admin.register(Chat)
+class ChatAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "created")
+    list_filter = ("created",)
+    search_fields = ("user__username",)
+    inlines = [MessageInline]
