d5f5f53cf2f714fa0d8e2b1ae8eb15072b24779b
qmayker <qmayker9@gmail.com>
2026-05-26 18:20:31 +0300

    test

--- studAI/chat/models.py
+++ studAI/chat/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib import settings
 
 # Create your models here.
+
+
+class Chat(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="chats"
+    )
+    created = models.DateTimeField(auto_now_add=True)
+
+
+class Message(models.Model):
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE, related_name="messages")
+    content = models.TextField()
