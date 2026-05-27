7725461726c153177c50f159e5a11b6f892342ca
qmayker <qmayker9@gmail.com>
2026-05-27 16:57:09 +0300

    DELETE: ChatNameField class

--- studai/chat/fields.py
+++ studai/chat/fields.py
@@ -1,17 +1,4 @@
 from django.db import models
-
-class ChatNameField(models.CharField):
-    def __init__(self, *args, **kwargs):
-        kwargs.setdefault("max_length", 255)
-        kwargs.setdefault("blank", True)
-        super().__init__(*args, **kwargs)
-    
-    def pre_save(self, model_instance, add):
-        value = getattr(model_instance, self.attname)
-        if not value:
-            setattr(model_instance, self.attname, f"Chat")
-        print(f"name value: {value}")
-        return super().pre_save(model_instance, add)
     
 class RelatedIDField(models.PositiveIntegerField):
     def __init__(self, *args, **kwargs):
