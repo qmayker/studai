7972848b25a89856229a4e6d41c03e11e86a9207
qmayker <qmayker9@gmail.com>
2026-06-04 16:30:17 +0300

    ADD: TestAttempt, QuestionAttempt models

--- studai/quizess/models.py
+++ studai/quizess/models.py
@@ -5,6 +5,16 @@ from django.conf import settings
 
 
 class TestAtempt(models.Model):
-    user = models.OneToOneField(
+    user = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="attempt"
     )
+    chat = models.ForeignKey(
+        "chat.Chat", on_delete=models.CASCADE, related_name="attempts"
+    )
+
+
+class QuestionAttempt(models.Model):
+    question = models.ForeignKey(
+        "questions.Question", on_delete=models.CASCADE, related_name="attempts"
+    )
+    answer = models.CharField(max_length=1)
--- studai/studai/settings.py
+++ studai/studai/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "chat.apps.ChatConfig",
     "questions.apps.QuestionsConfig",
-    "quizess.apps.QuiezessConfig",
+    "quizess.apps.QuizessConfig",
     "rest_framework",
 ]
 
