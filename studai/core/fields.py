959704a4ca552e3954aec83a068b612ab917c049
qmayker <qmayker9@gmail.com>
2026-06-24 13:59:59 +0300

    fields.py ADD: MultipleFileField to support multiple file select

--- studai/core/fields.py
+++ studai/core/fields.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django import forms
 
 
 class RelatedIDField(models.PositiveIntegerField):
@@ -25,3 +26,21 @@ class RelatedIDField(models.PositiveIntegerField):
 
 def get_path(instance, filename: str):
     return f"user_{instance.user.id}/{filename}"
+
+
+class MultipleFileInput(forms.ClearableFileInput):
+    allow_multiple_selected = True
+
+
+class MultipleFileField(forms.ImageField):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("widget", MultipleFileInput(attrs={"class": "upload"}))
+        super().__init__(*args, **kwargs)
+
+    def clean(self, data, initial=None):
+        single_file_clean = super().clean
+        if isinstance(data, (list, tuple)):
+            result = [single_file_clean(d, initial) for d in data]
+        else:
+            result = [single_file_clean(data, initial)]
+        return result
