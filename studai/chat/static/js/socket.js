96b07b44928086d98bcb758985c6cf57236db093
qmayker <qmayker9@gmail.com>
2026-07-01 18:04:11 +0300

    UPDATE js: lock/unlock button

--- studai/chat/static/js/socket.js
+++ studai/chat/static/js/socket.js
@@ -1,6 +1,8 @@
 export const socketPromise = new Promise((resolve) => {
     function initSocket() {
         const id = document.querySelector(".chat-detail").id;
+        const btn_class = document.querySelector(".send-button");
+        const btn = btn_class.querySelector("button");
 
         const chatSocket = new WebSocket(
             'ws://' +
@@ -9,14 +11,29 @@ export const socketPromise = new Promise((resolve) => {
             id +
             '/'
         );
+        console.log("WebSocket connection established.");
 
         chatSocket.onmessage = function (e) {
             const data = JSON.parse(e.data);
+            console.log(data);
 
             if (data.channel_id) {
                 resolve(data.channel_id);
             }
 
+            if (data["button-locked"] === false) {
+                const p = document.createElement("p");
+                p.textContent = "Uploading images...";
+                btn.style.display = "none";      
+                btn_class.appendChild(p);
+            } 
+            else if (data["button-locked"] === true) {
+                alert("Wait for previous images to upload before sending new ones.");
+            }
+            else if (data["button-finished"] === true) {
+                btn_class.removeChild(btn_class.querySelector("p"));
+                btn.style.display = "block";
+            }
         };
 
         chatSocket.onclose = function () {
